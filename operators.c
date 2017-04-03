8ddce7c7d80eff421b02fd9f98bc31c971848c39
KoKyuseok <onine94@hanmail.net>
2017-04-03 19:39:25 +0900

    fix bugs

--- operators.c
+++ operators.c
@@ -9,8 +9,7 @@ int minus(int op1, int op2) {
 int mul(int op1, int op2) {
 	return op1*op2;
 }
-
-int div(int op1, int op2) {
-	return op1%op2;
+float div(int op1, int op2) {
+	return (float)op1/op2;
 }
 
